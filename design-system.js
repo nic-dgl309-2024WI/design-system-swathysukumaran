fca94b5cb8b58a7d71e253687848a54a49b57d8b
swathysukumaran <swathysukumaran@gmail.com>
2024-02-04 16:19:40 -0800

    Style active state of links

--- design-system.js
+++ design-system.js
@@ -6,3 +6,40 @@ function toggleMobileMenu() {
         x.style.display = "block";
     }
 }
+
+//Refered https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_active_element
+
+var topNav = document.getElementById("nav-menu");
+var links = topNav.getElementsByClassName("u-no-style-link");
+console.log(links);
+
+for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener("click", function (index) {
+        return function () {
+            // Remove the "active" class from all elements
+            for (var j = 0; j < links.length; j++) {
+                links[j].classList.remove("active");
+            }
+
+            // Add the "active" class to the clicked element
+            links[index].classList.add("active");
+        };
+    }(i));
+}
+
+var sideNav = document.getElementById("side-menu");
+var sideLinks = sideNav.getElementsByClassName("u-no-style-link");
+
+for (var i = 0; i < sideLinks.length; i++) {
+    sideLinks[i].addEventListener("click", function (index) {
+        return function () {
+            // Remove the "active" class from all elements
+            for (var j = 0; j < sideLinks.length; j++) {
+                sideLinks[j].classList.remove("active");
+            }
+
+            // Add the "active" class to the clicked element
+            sideLinks[index].classList.add("active");
+        };
+    }(i));
+}
