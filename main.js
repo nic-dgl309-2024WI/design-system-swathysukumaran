3564213b15e5bec799438f2d7f63dce26235aba2
swathysukumaran <swathysukumaran@gmail.com>
2024-04-02 21:09:33 -0700

    Fix BEM

--- main.js
+++ main.js
@@ -11,8 +11,8 @@ function currentSlide(n) {
 
 function showSlides(n) {
     let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
+    let slides = document.getElementsByClassName("c-slideshow-container__mySlides");
+    let dots = document.getElementsByClassName("c-slideshow-container__dot");
     if (n > slides.length) { slideIndex = 1 }
     if (n < 1) { slideIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
